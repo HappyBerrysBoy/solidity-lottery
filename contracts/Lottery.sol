3441ce6bef1ba3cc8be9f9c16b0a222863491ed8
happyberrysboy <elytselyts@gmail.com>
2021-01-12 07:29:43 +0900

    working..

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -19,7 +19,7 @@ contract Lottery{
 
   // public 으로 전역변수 설정시 자동으로 getter을 만들어 준다
   // smart contract 외부에서 오너 확인 가능하다고 함
-  address public owner;
+  address payable public owner;
   uint256 private _tail;
   uint256 private _head;
   // _bets 변수에 베팅된 값들을 저장, 위에 선언된 _tail 값을 증가시키면서 저장하고, _head 값으로 베팅값을 검증한다
@@ -32,17 +32,36 @@ contract Lottery{
   uint256 constant internal BLOCK_LIMIT = 256;
 
   uint256 private _pot;
+  bool private mode = false;  // false : use answer for teset, true : real block hash 
+  bytes32 answerForTest;
 
   event BET(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
+  event WIN(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+  event FAIL(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+  event DRAW(uint256 index, address bettor, uint256 amount, byte challenges, byte answer, uint256 answerBlockNumber);
+  event REFUND(uint256 index, address bettor, uint256 amount, byte challenges, uint256 answerBlockNumber);
 
   constructor() public {
-    owner = msg.sender;
+    owner = msg.sender; // 21000GAS * gasPrice
   }
 
   // view는 smartcontract에 저장된 값에 접근 시 사용
   // pure는 컨트랙 접근 없이 사용 가능함 함수
   function getPot() public view returns(uint256 pot){
     return _pot;
+  }
+
+  /**
+    * @dev 베팅과 정답체크를 한다.
+    * @param challenges 유저가 베팅하는 글자
+    * @return 함수가 잘 수행 되었는지 확인하는 boolean값
+   */
+  function betAndDistribute(byte challenges) public payable returns (bool result){
+    bet(challenges);
+
+    distribute();
+
+    return true;
   }
 
   // Bet
@@ -68,13 +87,19 @@ contract Lottery{
   // _bets 값에다가 베팅 정보 저장
 
   // Distribute
+  /**
+    * @dev 베팅결과값을 확인하고 팟머니를 분배
+    * 정답 실패:팟머니 축적, 맞춤 : 팟머니 획득, 한글자 맞춤 : 베팅금액만 반환, 정답확인 불가 : 베팅 금액만 반환
+   */
   function distribute() public {
     // 큐에 저장된 베팅 정보가 (head)3, 4, 5, 6, 7, 8, 9, 10(tail)
     // 큐 순서대로 값들을 체크하고, 2자리 모두 맞힌 경우 팟머니를 전송,
     // 1자리만 맞힌 경우 건 값만 돌려줌
     uint256 cur;
+    uint256 transferAmount;
     BetInfo memory b;
     BlockStatus currentBlockStatus;
+    BettingResult currentBettingResult;
 
     for(cur=_head; cur<_tail; cur++){
       b = _bets[cur];
@@ -82,11 +107,39 @@ contract Lottery{
       
       // checkable : 블럭 넘버가 정답 블럭보다 커야하고, 블럭.number < BLOCK_LIMIT(256) + AnswerBlockNumber 
       if(currentBlockStatus == BlockStatus.Checkable){
+        bytes32 answerBlockHash = getAnswerBlockHash(b.answerBlockNumber);
+        currentBettingResult = isMatch(b.challenges, answerBlockHash);
         // if win, bettor gets pot
+        if(currentBettingResult == BettingResult.Win){
+          uint256 tmpPot = _pot;  // 총 수량 임시 저장
+
+          // pot = 0, pot을 0으로 만드는 작업을 가장 먼저 하는 것이 좋다. 
+          // 그렇지 않으면 전송되는 동안 _pot 값이 아직 0이 안된 상태라서 무슨 일이 생길지.!!
+          _pot = 0;
+
+          // transfer pot money
+          transferAmount = transferAfterPayingFee(b.bettor, tmpPot + BET_AMOUNT);
+          
+          // emit win event
+          emit WIN(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+        }
 
         // if fail, bettor's money goes pot
+          if(currentBettingResult == BettingResult.Fail){
+          // pot = pot + BET_AMOUNT
+          _pot += BET_AMOUNT;
+          // emit fail event
+          emit FAIL(cur, b.bettor, 0, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+        }
 
         // if draw, refund bettor's money
+          if(currentBettingResult == BettingResult.Draw){
+          // transfer only BET_AMOUNT
+          transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
+
+          // emit draw event
+          emit DRAW(cur, b.bettor, transferAmount, b.challenges, answerBlockHash[0], b.answerBlockNumber);
+        }
         
       }
 
@@ -99,12 +152,46 @@ contract Lottery{
       // block limit passed : block.number >= AnswerBlockNumber + BLOCK_LIMIT(256)
       if(currentBlockStatus == BlockStatus.BlockLimitPassed){
         // 환불
+          transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
         // emit refund event
+          emit REFUND(cur, b.bettor, transferAmount, b.challenges, b.answerBlockNumber);
       }
 
       popBet(cur);
       // check the answer
     }
+
+    _head = cur;
+  }
+
+  function transferAfterPayingFee(address payable addr, uint256 amount) internal returns(uint256){
+    // uint256 fee = amount / 100;
+    uint256 fee = 0;  // test를 위해서 0
+    uint256 amountWithoutFee = amount - fee;
+
+    // transfer to addr
+    addr.transfer(amountWithoutFee);
+
+    // transfer to owner
+    owner.transfer(fee);
+
+    // 스마트컨트랙트 안에서 ether를 전송하는 3가지 방법
+    // call, send, transfer(recommended)
+    // transfer : 이더 전송에만 사용됨. 전송 실패시 트랜잭션 자체가 fail처리됨(가장 안전함)
+    // send : 돈을 보내긴 하는데, boolean으로 결과를 리턴
+    // call : 이더를 보내거나 외부의 다른 스마트컨트랙트의 특정 function 호출이 가능(주로 여기서 문제가 많이 발생 함. 외부꺼 호출하다 취약점 발생)
+
+    return amountWithoutFee;
+  }
+
+  function setAnswerForTest(bytes32 answer) public returns (bool result) {
+    require(msg.sender == owner, "Only owner can set the answer for test mode");
+    answerForTest = answer;
+    return true;
+  }
+
+  function getAnswerBlockHash(uint256 answerBlockNumber) internal view returns(bytes32 answer){
+    return mode ? blockhash(answerBlockNumber) : answerForTest;
   }
 
 /**
